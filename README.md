110380ceaca2f1bf7feee728ed9a0b1da5887228
Temperest <chawanwit.jrssw@gmail.com>
2022-04-18 16:21:09 +0700

    create calculator skelton on index file and make-up with css      build function operate on script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # calculator
+
+Building calculator with javscript html and css
+    use
+    html create button for javascript to apply calculate 
+    javascript function to create operator
+        
---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("good")
+
+function add(a,b){
+    return a+b
+}
+function subtrac(a,b){
+    return a-b
+}
+function multiplay(a,b){
+    return a*b
+}
+function divide(a,b){
+    return a/b
+}
+function operate(a,b){
+    add(a,b)
+    subtrac(a,b)
+    multiplay(a,b)
+    divide(a,b)
+}
