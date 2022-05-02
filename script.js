30b27c86e4696d082a811060d399067808f9ddaf
Temperest <chawanwit.jrssw@gmail.com>
2022-05-02 17:22:13 +0700

    script.js  improve function operator ,         add function to display number properly in browser

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 console.log("good")
 
-function add(a,b){
-    return a+b
+function add(a, b) {
+    return a + b
 }
-function subtract(a,b){
-    return a-b
+function subtract(a, b) {
+    return a - b
 }
-function multiply(a,b){
-    return a*b
+function multiply(a, b) {
+    return a * b
 }
-function divide(a,b){
-    if (b == 0){
+function divide(a, b) {
+    if (b == 0) {
         return noDizero()
     }
-    return a/b
+    return a / b
 }
-function operate(operator,a,b){
-    switch (operator){
-    case '+' : return add(a,b)
-        break;
-    case '-' : return subtract(a,b)
-        break;
-    case '*' : return multiply(a,b)
-        break;
-    case '/' : return divide(a,b)
-        break;
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+': return add(a, b)
+            break;
+        case '-': return subtract(a, b)
+            break;
+        case '*': return multiply(a, b)
+            break;
+        case '/': return divide(a, b)
+            break;
     }
 }
-function noDizero () {
+function noDizero() {
     alert("Division by zero is undifined");
 }
 const btnNum = document.querySelectorAll('.number');
@@ -39,12 +39,16 @@ let valueA = 0;
 let valueB = 0;
 let total = 0;
 let inputArray = [];
-let operator = "+";
+let inputOperator = "";
+let operator = "";
+let topBar = document.querySelector('.topBar');
+let lowBar = document.querySelector('.lowBar');
 
 btnNum.forEach((btn) => {
     btn.addEventListener('click', () => {
-        alert(btn.innerText);
+        // alert(btn.innerText);
         inputArray.push(btn.innerText);
+        loDis(inputArray);
     })
 })
 
@@ -53,45 +57,86 @@ const btnOper = document.querySelectorAll('.operate');
 
 btnOper.forEach((btn) => {
     btn.addEventListener('click', () => {
-        alert(btn.innerText);
-        operator = btn.innerText;
+        // alert(btn.innerText);
+        inputOperator = btn.innerText;
 
-        if (arrayValueA.length >0 && arrayValueB.length >0){
-            valueA = Number(arrayValueA.join(''));
-            valueB = Number(arrayValueB.join(''));
-            total = operate(operator,valueA,valueB);
-        }
-
-        if (arrayValueA.length == 0 ){
-        arrayValueA = inputArray.slice();
-        inputArray.length = 0;
+        if (arrayValueA.length == 0) {
+            arrayValueA = inputArray.slice();
+            inputArray.length = 0;
         }
         else {
             arrayValueB = inputArray.slice();
             inputArray.length = 0
-        }
+            valueA = Number(arrayValueA.join(''));
+            valueB = Number(arrayValueB.join(''));
+            operator = inputOperator
 
+            total = operate(operator,valueA,valueB);
+
+            arrayValueA = Array.from(String(total), Number);
+
+            upDis(arrayValueA,operator)
+            loDis(total);
+            return
+        }
+        upDis(arrayValueA,inputOperator,[])
     })
 })
 
 const btnEqu = document.querySelector('.equal');
 
 btnEqu.addEventListener('click', () => {
-    alert(btnEqu.innerText);
+    // alert(btnEqu.innerText);
+    arrayValueB = inputArray.slice();
+    inputArray.length = 0
+    valueA = Number(arrayValueA.join(''));
+    valueB = Number(arrayValueB.join(''));
+    operator = inputOperator
+    upDis(arrayValueA,operator,arrayValueB)
+    total = operate(operator,valueA,valueB)
+    arrayValueA = Array.from(String(total), Number);
+    loDis(total);
 });
 
 const btnClear = document.querySelector('.clear');
 
 btnClear.addEventListener('click', () => {
-    alert(btnClear.innerText);
+    // alert(btnClear.innerText);
+    arrayValueA = [];
+    arrayValueB = [];
+    valueA = 0;
+    valueB = 0;
+    total = 0;
+    inputArray = [];
+    inputOperator = "";
+    operator = "";
+    topBar.innerText = '0';
+    lowBar.innerText = '0';
 });
 
 const btnUndo = document.querySelector('.undo');
 
 btnUndo.addEventListener('click', () => {
-    alert(btnUndo.innerText);
+    // alert(btnUndo.innerText);
+    inputArray.splice(-1);
+    loDis(inputArray);
 });
 
-function displayNumber (dis1,dis2){
+function upDis(dis1,dis2,dis3) {
+    if ( Array.isArray(dis3)){
+    topBar.innerText = dis1.join("") + dis2 + dis3.join('');
+    }else {
+    topBar.innerText = dis1.join("") + dis2;
+    }
 
 }
+
+function loDis(disa,disb) {
+    if ( Array.isArray(disa) ){
+        lowBar.innerText = disa.join("");
+    } else {
+        lowBar.innerText = disa
+    }
+}
+
+// document.querySelector(".lowBar").innerText = "456"
