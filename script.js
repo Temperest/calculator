b4d585746085d87f1674c1164d154c76b7bbc57e
Temperest <chawanwit.jrssw@gmail.com>
2022-05-07 16:08:06 +0700

    add keyboard support

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ function calculator() {
     }
 }
 
-const btnClear = document.querySelector('.clear');
+const btnClear = document.querySelector('#clear');
 
 btnClear.addEventListener('click', () => {
     clear();
@@ -91,7 +91,7 @@ function clear(){
     operator = "";
 }
 
-const btnUndo = document.querySelector('.undo');
+const btnUndo = document.querySelector('#undo');
 btnUndo.addEventListener('click', () => {
     inputArray.splice(-1);
     loDis(inputArray);
@@ -161,7 +161,87 @@ function round(num) {
  
 
 // work in process //
-window.addEventListener('keydown', (e) => {
-    const key = document.querySelector(`button[class="${e.key}"]`)
+window.onkeydown = function(e){
+    let x = e.key;
+    let choice
+    switch(x){
+        case '1':
+            choice = document.querySelector('#one');
+            choice.click();
+            break;
+        case '2':
+            choice = document.querySelector('#two');
+            choice.click();
+            break;
+        case '3':
+            choice = document.querySelector('#three');
+            choice.click();
+            break;
+        case'4':
+            choice = document.querySelector('#four');
+            choice.click();
+            break;
+        case '5':
+            choice = document.querySelector('#five');
+            choice.click();
+            break;
+        case '6':
+            choice = document.querySelector('#six');
+            choice.click();
+            break;
+        case '7':
+            choice = document.querySelector('#seven');
+            choice.click();
+            break;
+        case '8':
+            choice = document.querySelector('#eight');
+            choice.click();
+            break;
+        case '9':
+            choice = document.querySelector('#nine');
+            choice.click();
+            break;
+        case '0':
+            choice = document.querySelector('#zero');
+            choice.click();
+            break;
+        case 'Escape':
+            choice = document.querySelector('#clear');
+            choice.click();
+            break;
+        case 'Backspace':
+            choice = document.querySelector('#undo');
+            choice.click();
+            break;
+        case '^':
+            choice = document.querySelector('#exp');
+            choice.click();
+            break;
+        case '/':
+            choice = document.querySelector('#divide');
+            choice.click();
+            break;
+        case '*':
+            choice = document.querySelector('#multiply');
+            choice.click();
+            break;
+        case '-':
+            choice = document.querySelector('#subtract');
+            choice.click();
+            break;
+        case '+':
+            choice = document.querySelector('#add');
+            choice.click();
+            break;
+        case '.':
+            choice = document.querySelector('#decimal');
+            choice.click();
+            break;
+        case 'Enter':
+            choice = document.querySelector('#enter');
+            choice.click();
+            break;
+        default:
+    }
+}
     // console.log(e)
-})
